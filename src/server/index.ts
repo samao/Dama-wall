5f3dcd982792709b9ffbd22c0b3c9aaee044405d
samao <qiyanlong@wozine.com>
2017-09-19 12:09:52 +0800

    install dependencies

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,8 @@
 import * as express from "express";
 import {log} from 'util';
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+//import * as multer from "multer";
 
 const app = express();
 
@@ -11,6 +14,21 @@ const server = app.listen(3000,() => {
     const {address,port} = server.address();
     log(`服务器启动: http://${address}:${port}`);
 });
+
+//json 化数据 application/json
+app.use(bodyParser.json());
+// application/x-www-form-url
+app.use(bodyParser.urlencoded({extended:true}));
+// 获取请求cookie
+app.use(cookieParser());
+//multipart/form-data
+//app.use(multer());
+
+//静态资源
+app.use('static',express.static('public'));
+//模板路径
+app.set('views','./views');
+app.set('view engine','pug');
 
 import('./user').then(mod => {
     const {User} = mod;
