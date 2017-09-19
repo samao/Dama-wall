b87892f950bb9c7e9ea1747bbd79edfe93770d83
samao <qiyanlong@wozine.com>
2017-09-19 11:42:11 +0800

    add npm scripts

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,8 +7,13 @@ app.route('/').get((req, res, next) => {
     res.end('Hello Express');
 });
 
-
 const server = app.listen(3000,() => {
     const {address,port} = server.address();
     log(`服务器启动: http://${address}:${port}`);
+});
+
+import('./user').then(mod => {
+    const {User} = mod;
+    let {name,age} = new User('王胖子',31);
+    log(`发现个肉货:${name},年龄：${age}`);
 })
