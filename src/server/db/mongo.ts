5020f43d472b09998ef57299dc413f314a184e1f
samao <qiyanlong@wozine.com>
2017-09-25 17:24:14 +0800

    test

--- src/server/db/mongo.ts
+++ src/server/db/mongo.ts
@@ -5,24 +5,38 @@ import { MongoClient as mongo } from "mongodb";
 
 import { promisify } from "util";
 
-const dburl = `mongodb://localhost:${ports.db}/dama`;
+const dburl = `mongodb://localhost:${ports.db}`;
 
-const connect = async () => {
-   return await promisify(mongo.connect)(dburl,{}).catch(reason => error('数据库链接失败',reason));
-}
-
-const insert = async (data:any) => {
-    let db = await connect();
+const connect = async (col: string) => {
+    let db = await promisify(mongo.connect)(`${dburl}/${col}`,{}).catch(reason => error('数据库链接失败',reason));
     if(db) {
-        log('数据库链接成功');
-        db.close();
+        let collection = db.collection(col);
+        return {db, collection};
     }
 }
-const update = async (data: any) => {
+
+const insert = async (col: string, data: any) => {
+    let db = await connect(col);
+    if(db) {
+        let {result} = await db.collection.insert({name:'samao',pwd:'samao'});
+        if(result.ok) {
+            log('完成');
+        }else{
+            error('失败');
+        }
+        db.db.close();
+    }
+}
+const update = async (col: string, data: any) => {
 
 }
-const find = async (data: any) => {
-    
+const find = async (col: string, data?: any) => {
+    let db = await connect(col);
+     if(db) {
+        let result = await db.collection.find(data).toArray();
+        log('获取到得数组：',JSON.stringify(result));
+        db.db.close();
+    }
 }
 const remove = async (data: any) => {
     
