4623d0513ecd76923cb60b4d5e103152f95fc1d3
samao <qiyanlong@wozine.com>
2017-09-26 17:49:25 +0800

    全局安装日志修改

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "==== 安装项目依赖"
-echo "全局工具安装"
-npm install -g webpack typescript
-echo "项目模块安装"
+echo "(1.全局工具安装"
+sudo npm install -g webpack typescript
+echo "(2.项目模块安装"
 npm install
 echo "==== 依赖安装完成"
