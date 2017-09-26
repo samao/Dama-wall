99ff46c8dc2821c0090fe254dafb420f397efcff
samao <qiyanlong@wozine.com>
2017-09-26 16:53:58 +0800

    分割命令

--- bin/backup.sh
+++ bin/backup.sh
@@ -7,7 +7,3 @@ zip -r data/db.zip data/db_back/*
 echo "*** 删除暂存文件 ***"
 rm -r data/db_back
 echo "*** 备份完毕 ***"
-echo "=== 提交备份到github ==="
-#定义时间
-now=`date`
-. bin/push.sh "'数据库备份：'$now"
--- bin/push.sh
+++ bin/push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #定义时间
+echo "=== 提交备份到github ==="
 git add --all
 git commit -a --message "$1"
 git push origin master
