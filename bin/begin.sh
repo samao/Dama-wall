3744736071f3a82bfbcffeaae63f03944910bdb9
samao <qiyanlong@wozine.com>
2017-09-26 16:38:52 +0800

    原始数据库内容

--- bin/begin.sh
+++ bin/begin.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo ''$1
+echo "=== 暂时关闭数据库"
+brew services stop mongodb
+echo "==== 拉取最新代码"
+git checkout -f
+echo "==== 删除数据库"
+rm -r data/db
+mkdir data/db
+echo "==== 重启数据库"
+brew services start mongodb
+. bin/restore.sh
