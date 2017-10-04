9b58370f2850dc45fca9be5bdf69ece99c8810f0
samao <qiyanlong@wozine.com>
2017-10-04 23:50:26 +0800

    注册信息前端验证

--- src/browser/register.ts
+++ src/browser/register.ts
@@ -1,9 +1,30 @@
 import * as $ from 'jquery';
+import { log, error } from "../utils/log";
+
 $(() => {
     $('#registerBtn').click(function(){
+        let username = $('input[name="username"]').val();
+        let pwd = $('input[name="pwd"]').val();
+        let confirm = $('input[name="confirmPwd"]').val();
+
+        if(typeof username !== 'string' || typeof pwd !== 'string' || typeof confirm !== 'string') return;
+
+        if(username.trim().length < 4) {
+            error('用户名不能少于4个字符')
+            return;
+        }
+        if(pwd.trim().length < 5) {
+            error('密码太简单了')
+            return;
+        }
+        if(pwd.trim() !== confirm.trim()) {
+            error('两次输入的密码不一致')
+            return;
+        }
+
         $.post('/register',{
-            username:$('input[name="username"]').val(),
-            pwd:$('input[name="pwd"]').val()
+            username,
+            pwd
         }, data => {
             console.log(data)
             if(data.ok) {
