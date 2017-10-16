c5cb8f851feccc413a8a3e8b8a1e0522c0ae227a
samao <qiyanlong@wozine.com>
2017-10-16 19:05:21 +0800

    发送表情弹幕前端处理

--- src/browser/send.ts
+++ src/browser/send.ts
@@ -1,7 +1,8 @@
 import * as $ from 'jquery';
 $(() => {
+    let input = $('input[type="text"]');
+
     $('#sendBtn').click(function(){
-        let input = $('input[type="text"]');
         $.post(location.href, {message:input.val()},(data) => {
             data = JSON.parse(data);
             if(data.ok) 
@@ -11,5 +12,31 @@ $(() => {
             $('#info').fadeIn().fadeOut(3000);
         })
         input.val('');
+    })
+
+    let hiden: boolean = true;
+    const emoj = $('.emojPanel');
+
+    $('.face-toggle').click(() => {
+        if(hiden) {
+            emoj.show();
+        }else{
+            emoj.hide();
+        }
+        hiden = !hiden;
+    });
+
+    $('.emojPanel li').click(function() {
+        emoj.hide();
+        const msg = input.val();
+        if(typeof msg === 'string') {
+            let tag = $(this).attr('data-tag')||'';
+            const dom: any = input.get(0);
+            const begin: number = dom.selectionStart;
+            const end: number = dom.selectionEnd;
+            let olds = msg.split('');
+            olds.splice(begin, end - begin,tag);
+            input.val(olds.join(''));
+        }
     })
 })
