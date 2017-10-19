ea32221fac3a11d4f90a7953bac16fc65f806630
samao <qiyanlong@wozine.com>
2017-10-19 18:02:08 +0800

    二维码生成接口挂起修复

--- src/server/route/api.ts
+++ src/server/route/api.ts
@@ -1,12 +1,11 @@
 import * as express from "express";
 import * as QRCode from "qrcode";
 import * as path from 'path';
-import { createWriteStream } from "fs";
 
 import { checkout, restore } from "../db/pool";
 import { Collection } from "../db/collection";
 import { log, error } from "../../utils/log";
-import { HOST } from "../config/conf";
+import { HOST, ports } from "../config/conf";
 import { success, failure } from "../../utils/feedback";
 
 const router = express.Router();
@@ -14,6 +13,7 @@ const router = express.Router();
 //提供哦前端数据接口
 router.route('/activity/:rid').all((req, res,next) => {
     //分权限
+    log(`本次rid: ${req.params.rid}`)
     next();
 }).post((req, res, next) => {
     //新建活动
@@ -35,16 +35,20 @@ function createAct(req:IRequest, res:IRespond): void {
     //1.活动写入数据库
     checkout(db => {
         db.collection(Collection.ACTIVITY).insert({rid:req.params.rid}).then(result => {
-            log('写入数据库成功',JSON.stringify(result))
+            log('活动信息成功写入数据库')
             //2.生成二维码文件
-            const QRStream = createWriteStream(path.resolve('public','images','qr',`${req.params.rid}.png`));
-            QRCode.toFileStream(QRStream,`${HOST}/danmu/${req.params.rid}`, err => {
-                if(err) {
-                    failure(res, `生成二维码失败 ${err}`)
-                }else{
-                    success(res);
-                }
-            })
+            const QRPath = path.resolve('public','images','qr',`${req.params.rid}.png`);
+            const QRData = `${HOST}/danmu/${req.params.rid}`;
+            
+            log('二维码存储地址',QRPath);
+            log('二维码内容',QRData);
+
+            QRCode.toFile(QRPath, QRData, err => {
+                if(err)
+                    failure(res, `生成二维码失败 ${err.message}`);
+                else
+                    success(res, `${HOST}:${ports.web}/qr/${req.params.rid}`);
+            });
         },({errmsg:reason}) => {
             failure(res, `活动名称已经存在 ${req.params.rid}`)
         });
