4ce4f8d766bd28cda6ea9c57fe7a6390ec122b63
samao <qiyanlong@wozine.com>
2017-10-26 17:27:33 +0800

    dot模板文件不存在跳404页面

--- src/server/route/template.ts
+++ src/server/route/template.ts
@@ -1,5 +1,7 @@
+import { exists } from 'fs';
 import * as express from "express";
 import * as path from 'path';
+import * as util from 'util'
 
 import { log, error } from "../../utils/log";
 import { failure, success } from "../../utils/feedback";
@@ -7,10 +9,20 @@ import { compileFile } from 'pug';
 
 const router = express.Router();
 
+async function existPug(url: string) {
+    return await util.promisify(exists)(url);
+}
+
 router.route('/:name').get((req, res, next) => {
-    log('模板路径',path.resolve('public','template',req.params.name + '.pug'));
-    const compileMethod = compileFile(path.resolve('public','template',req.params.name + '.pug'));
-    res.end(compileMethod());
+    const pugPath = path.resolve('public','template',req.params.name + '.pug');
+    existPug(pugPath).then(bool => {
+        if(bool) {
+            const compileMethod = compileFile(pugPath);
+            res.end(compileMethod());
+        }else {
+            next();
+        }
+    })
 })
 
 export default router;
