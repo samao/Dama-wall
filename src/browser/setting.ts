a86139733437a2e13622be87f420c42d1695f973
samao <qiyanlong@wozine.com>
2017-12-29 17:37:05 +0800

    前端用户中心react框架。

--- src/browser/setting.ts
+++ src/browser/setting.ts
@@ -1,13 +1,14 @@
 /*
  * @Author: iDzeir 
  * @Date: 2017-12-19 12:28:33 
- * @Last Modified by:   iDzeir 
- * @Last Modified time: 2017-12-19 12:28:33 
+ * @Last Modified by: iDzeir
+ * @Last Modified time: 2017-12-29 17:35:39
  */
 
 import * as $ from 'jquery';
 import { log, error } from "../utils/log";
 import { loadPug } from './utils/pugUtil';
+import * as React from 'react';
 
 $(() => {
     const container = $('.wraper');
--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,8 @@ module.exports = {
         })
     ],
     externals: {
-        'jquery':'jQuery'
+        'jquery':'jQuery',
+        'react':"React",
+        'react-dom':'ReactDOM'
     }
 }
